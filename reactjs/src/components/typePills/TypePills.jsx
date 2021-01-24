6b21e2e8b4ae20d056f86968657a81813b846d6c
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-01-24 12:29:04 -0800

    added dynamic img path, imported react image - added default props

--- reactjs/src/components/typePills/TypePills.jsx
+++ reactjs/src/components/typePills/TypePills.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Image from 'react-bootstrap/Image';
 import './TypePills.scss';
 
-const TypePills = ({ type, name }) => {
+const TypePills = ({ type, name, variant }) => {
+	// disabling eslint on next line - using .default to ensure it's not bringing in a module and using a dynamic import from the prop
+	// also to allow require not at the top of the file. This image is dependent on the prop and cannot be at the top of the file
+	// eslint-disable-next-line
+	const imgPath = require(`../../assets/images/icons/${type}.svg`);
 	return (
-		<button type="button" className="TypePills">
-			{type}
-			{name}
+		<button
+			type="button"
+			className={`TypePills ${variant ? 'round' : 'pill'} ${type}`}
+		>
+			<Image src={imgPath.default} />
+			{!variant && name}
 		</button>
 	);
 };
 
 TypePills.propTypes = {
 	type: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
+	name: PropTypes.string,
+	variant: PropTypes.string,
+};
+
+TypePills.defaultProps = {
+	variant: 'pill',
+	name: 'normal',
 };
 
 export default TypePills;
