43f48ee434616e70bf32df2f7ef019b02fde8342
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-01-23 18:27:20 -0800

    moved base styles into assets scss folder - set up base overrides for bootstrap - added bootstrap navbar to header

--- reactjs/src/components/header/Header.jsx
+++ reactjs/src/components/header/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './Header.scss';
 
 const Header = () => {
-	return <div className="Header" />;
+	return (
+		<header className="Header">
+			<Navbar bg="primary" expand="lg" variant="dark">
+				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Nav.Link className="px-4" href="#home">
+							Dashboard
+						</Nav.Link>
+						<Nav.Link className="px-4" href="#link">
+							Profile
+						</Nav.Link>
+						<Nav.Link className="px-4" href="#link">
+							Logout
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
