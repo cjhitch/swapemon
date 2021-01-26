4eeee9c65c173daadb706b06357c9d9916a862b3
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-01-26 00:38:50 -0800

    set up ternary and added more prop types for rest of form controls

--- reactjs/src/components/formControl/FormControl.jsx
+++ reactjs/src/components/formControl/FormControl.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
 import './FormControl.scss';
 
-const FormControl = ({ value, update }) => {
+const FormControl = ({ value, update, type, id, label, name, options }) => {
 	return (
 		<div className="FormControl">
-			{value}
-			{update}
+			{type === 'check' ? (
+				<Form.Check
+					type={type}
+					id={`${type}-${id}`}
+					value={value}
+					checked={value}
+					onChange={(e) => update(id, e.target.value)}
+				/>
+			) : label ? (
+				<Form.Group>
+					<Form.Label>{label}</Form.Label>
+					<Form.Control as={type} placeholder={name}>
+						{options &&
+							options.map((opt) => (
+								<option key={opt.name} value={opt.id} />
+							))}
+					</Form.Control>
+				</Form.Group>
+			) : (
+				<Form.Control as={type} placeholder={name}>
+					{options &&
+						options.map((opt) => (
+							<option key={opt.name} value={opt.id} />
+						))}
+				</Form.Control>
+			)}
 		</div>
 	);
 };
@@ -18,6 +43,18 @@ FormControl.propTypes = {
 		PropTypes.bool,
 	]).isRequired,
 	update: PropTypes.func.isRequired,
+	type: PropTypes.string.isRequired,
+	id: PropTypes.string,
+	label: PropTypes.string,
+	name: PropTypes.string,
+	options: PropTypes.arrayof(PropTypes.string),
+};
+
+FormControl.defaultProps = {
+	id: null,
+	label: null,
+	name: null,
+	options: [],
 };
 
 export default FormControl;
