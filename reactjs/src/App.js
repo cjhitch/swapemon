aac30453104f200e4122b787194bacbb29e53276
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-01-29 00:44:49 -0800

    set up home page

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-// import React, { useState } from 'react';
-import Header from './components/header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+// import Header from './components/header';
 // import TypePills from './components/typePills';
 // import PokemonCard from './components/pokemonCard';
 // import Logo from './components/logo';
 // import FormControl from './components/formControl';
 // import Profile from './components/profile';
 // import Messages from './components/messages';
-import Trade from './components/trade';
-import Footer from './components/footer';
+// import Trade from './components/trade';
+// import Footer from './components/footer';
 import './App.scss';
 
 function App() {
@@ -25,35 +26,35 @@ function App() {
 	// };
 	// const [formState, setFormState] = useState(initialFormState);
 	// this will ultimately be replaced with redux and database
-	const pokemon = [
-		{
-			name: 'Charizard',
-			shiny: true,
-			dex: '006',
-			ball: 'dream',
-			level: '100',
-			types: [
-				{ type: 'fire', name: 'Fire' },
-				{ type: 'flying', name: 'Flying' },
-			],
-			gender: ['female', '87.5%'],
-			ability: 'Solar Power (Hidden)',
-			ivs: [
-				{ HP: 31 },
-				{ Atk: 'N/A' },
-				{ Def: 31 },
-				{ SpAtk: 'N/A' },
-				{ SpDef: 30 },
-				{ Spd: 30 },
-			],
-			eggMoves: [
-				{ normal: 'Belly Drum' },
-				{ dark: 'Bite' },
-				{ dragon: 'Dragon Tail' },
-				{ flying: 'Wing Attack' },
-			],
-		},
-	];
+	// const pokemon = [
+	// 	{
+	// 		name: 'Charizard',
+	// 		shiny: true,
+	// 		dex: '006',
+	// 		ball: 'dream',
+	// 		level: '100',
+	// 		types: [
+	// 			{ type: 'fire', name: 'Fire' },
+	// 			{ type: 'flying', name: 'Flying' },
+	// 		],
+	// 		gender: ['female', '87.5%'],
+	// 		ability: 'Solar Power (Hidden)',
+	// 		ivs: [
+	// 			{ HP: 31 },
+	// 			{ Atk: 'N/A' },
+	// 			{ Def: 31 },
+	// 			{ SpAtk: 'N/A' },
+	// 			{ SpDef: 30 },
+	// 			{ Spd: 30 },
+	// 		],
+	// 		eggMoves: [
+	// 			{ normal: 'Belly Drum' },
+	// 			{ dark: 'Bite' },
+	// 			{ dragon: 'Dragon Tail' },
+	// 			{ flying: 'Wing Attack' },
+	// 		],
+	// 	},
+	// ];
 	// const update = (inputId, value) => {
 	// 	console.log(inputId, value);
 	// 	if (inputId === 'published' || inputId === 'archived') {
@@ -68,8 +69,15 @@ function App() {
 	// };
 	return (
 		<div className="App">
-			<Header />
-			<Trade myTrades={pokemon} />
+			<Router>
+				<Switch>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+			{/* <Header />
+			<Trade myTrades={pokemon} /> */}
 			{/* <TypePills type="electric" name="Electric" />
 			<TypePills type="electric" variant="round" />}
 			{<PokemonCard pokemon={pokemon} />
@@ -97,7 +105,7 @@ function App() {
 			/>
 			<Profile />
 			<Messages /> */}
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	);
 }
