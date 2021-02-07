aa6daa1479100c207d0264fd1c3c68fa1eecee18
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-02-07 11:06:47 -0800

    set up model file for pokeballs

--- api/models/pokeballs.js
+++ api/models/pokeballs.js
@@ -1,24 +1,43 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pokeballs extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Pokeballs.init({
-    name: DataTypes.STRING,
-    sprite: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pokeballs',
-  });
-  return Pokeballs;
+	class Pokeballs extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Pokeballs.init(
+		{
+			name: {
+				defaultValue: DataTypes.STRING,
+				primaryKey: true,
+				allowNull: { args: false, msg: 'Pokeball name is required' },
+				type: DataTypes.STRING,
+				validate: {
+					len: {
+						args: [3, 70],
+						msg: 'Pokeball name must be at least 3 characters.',
+					},
+				},
+			},
+			sprite: {
+				allowNull: { args: false, msg: 'Pokeball sprite is required' },
+				allowNull: {
+					args: false,
+					msg: 'Pokeball sprite must be at least 3 characters',
+				},
+				type: DataTypes.STRING,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Pokeballs',
+		},
+	);
+	return Pokeballs;
 };
