c770c5ccce8871606567b07ff196e4a1bc098cc5
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-02-07 14:41:14 -0800

    created new migration and model file

---
+++ api/models/conversations.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Conversations extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Conversations.init({
+    id: DataTypes.UUID,
+    userId: DataTypes.UUID,
+    username: DataTypes.STRING,
+    msg: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Conversations',
+  });
+  return Conversations;
+};
