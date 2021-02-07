69400c76f5290d45728ec506d8c98b1b220f8fb6
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-02-07 11:10:27 -0800

    set up migration file for pokeballs

--- api/migrations/20210207190129-create-pokeballs.js
+++ api/migrations/20210207190129-create-pokeballs.js
@@ -1,30 +1,26 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Pokeballs', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      sprite: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Pokeballs');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Pokeballs', {
+			name: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.STRING,
+			},
+			sprite: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Pokeballs');
+	},
 };
