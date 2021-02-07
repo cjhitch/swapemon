4cae4577c434ebded2ea05e13f3d94afb2f2de70
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-02-07 14:49:26 -0800

    set up model and migration for conversations

--- api/migrations/20210207224103-create-conversations.js
+++ api/migrations/20210207224103-create-conversations.js
@@ -1,36 +1,36 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Conversations', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.UUID
-      },
-      userId: {
-        type: Sequelize.UUID
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      msg: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Conversations');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Conversations', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.UUID,
+			},
+			username: {
+				allowNull: false,
+				type: Sequelize.STRING,
+			},
+			convoMsg: {
+				allowNull: false,
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Conversations');
+	},
 };
