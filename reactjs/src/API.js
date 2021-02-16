b1d37a2de538312f640ea84a95940fe032d383b9
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-02-16 13:41:15 -0800

    attempting to use nodeenv to establish correct url for api

--- reactjs/src/API.js
+++ reactjs/src/API.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 require('dotenv').config();
 
 const API = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+	baseURL:
+		process.env.NODE_ENV === 'production'
+			? 'https://swapemon.herokuapp.com/'
+			: 'http://localhost:5000',
 });
 
 export default API;
